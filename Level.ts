b2aecb5fa9162efb9d29d93c13bcf06bd32aafa5
Eli Kosminsky <ekosmin@mit.edu>
2015-06-24 12:03:06 -0400

    Creating organized magnets

--- Level.ts
+++ Level.ts
@@ -3,14 +3,40 @@ module Main {
 
     export class Level extends Phaser.State {
 
-        public console: Phaser.Text;
+        public words: Phaser.Group;
+
+        private static words: string[] = ['boil', 'apparatus', 'away', 'and', 'above', 'frantic', 'enormous', 'cry',
+                                          'but', 'is', 'if', 'I', 'honey', 'her', 'girl', 'on', 'not', 'mad', 'ly',
+                                          'like', 'lie', 'the', 'still', 'some', 'she', 's', 's', 'rip', 'you',
+                                          'woman', 'why', 'use', 'time', 'the', 'you'];
 
         create() {
             this.game.stage.backgroundColor = '#777777';
 
             var style: {} = {font: "32px Arial", fill: "#FFFFFF", align: "left",
                 wordWrap: true, wordWrapWidth: this.game.width};
-            this.console = this.game.add.text(0, this.game.height/2, "", style);
+            var currentX: number = 0;
+            var currentY: number = 0;
+
+            for (var i: number = 0; i < Level.words.length; i++) {
+                var word: string = Level.words[i];
+                var text: Phaser.Text = this.game.add.text(0, 0, word, style, this.words);
+                this.placeText(text, currentX, currentY);
+                currentX = text.x + text.width;
+                currentY = text.y;
+                text.inputEnabled = true;
+                text.input.enableDrag();
+            }
+        }
+
+        private placeText(text: Phaser.Text, currentX: number, currentY: number): void {
+            if (currentX + text.width > this.game.width - 10) {
+                text.x = 10;
+                text.y = currentY + text.height + 10;
+            } else {
+                text.x = currentX + 10;
+                text.y = currentY;
+            }
         }
 
     }
